ff1fb2f19cab4d8b7cf5de560793d1d1d1b98db1
matrix-rf <adi.leibowitz@gmail.com>
2020-04-10 17:59:53 +0300

    updated ignore file

--- forms/CompleteDetails/formdata/CompleteDetails-0753471c-csv.php
+++ forms/CompleteDetails/formdata/CompleteDetails-0753471c-csv.php
@@ -3,4 +3,13 @@ _sfm_form_submision_time_,_sfm_unique_id_,SSN,_sfm_visitor_ip_,FirstName,LastNam
 4/8/2020 13:12:40 IDT(+0300 GMT),becceca5577bb8e7ccc209d4425e4db0,913234484,127.0.0.1,Hillel,Leibowitz,example_check.png
 4/8/2020 14:30:41 IDT(+0300 GMT),cadfef39e362af634e426746a9614eae,454224322,127.0.0.1,Hillel,Leibowitz,example_check1.png
 4/8/2020 15:43:48 IDT(+0300 GMT),253733d8dbf9ef3c429853fc200e8bb1,454224322,127.0.0.1,Hillel,Leibowitz,VoidChequeEN.jpg
+4/10/2020 13:48:11 IDT(+0300 GMT),e02cbda0a42fd7120ce4c2de6c23129a,352453444,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg22.png
+4/10/2020 14:07:42 IDT(+0300 GMT),97b36ca2c382aca549129fef035e42e8,352453442,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg23.png
+4/10/2020 14:15:54 IDT(+0300 GMT),455c01a29edf84e5e8786352dbf7f8ed,352453443,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg24.png
+4/10/2020 14:18:02 IDT(+0300 GMT),ac9501118ff92dd69e008bac68ca6146,232323567,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg25.png
+4/10/2020 14:40:30 IDT(+0300 GMT),8dd5d3c68fd76bb7dc7a9bbf5b29f5af,232343567,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg26.png
+4/10/2020 14:46:19 IDT(+0300 GMT),01a3460979f7ad63624c6e1ec6d97b2c,232343577,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg27.png
+4/10/2020 14:48:11 IDT(+0300 GMT),b55984ff7b41471de44512228571283a,772343577,127.0.0.1,Hillel,Leibowitz,1200px-IAI-Logo.svg28.png
+4/10/2020 15:09:23 IDT(+0300 GMT),3a4eeca8e73370bc5022b19da02d3ba7,683943233,127.0.0.1,Test,Again,1200px-IAI-Logo.svg29.png
+4/10/2020 15:13:21 IDT(+0300 GMT),98bae9bac3a0c0d443d9b88222745b9e,934586444,127.0.0.1,Test,Again,1200px-IAI-Logo.svg30.png
 --Simfatic Forms CSV File-- */ ?>
